9296e27bf7a0bdad505ee1c1f71745c994db5d02
Christopher Shockley <cishockleysr@gmail.com>
2020-08-03 20:48:30 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// get quote from API
+async function getQuote() {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    getQuote();
+    console.log("uhoh, no quote", err);
+  }
+}
+
+// on load
+getQuote();
